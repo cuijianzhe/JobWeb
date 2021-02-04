d80b06bcb6c3b8687129d1adc93e131f8d8f2895
cuijianzhe <598941324@qq.com>
2021-02-04 10:22:31 +0800

    在线提交简历

--- jobs/urls.py
+++ jobs/urls.py
@@ -7,10 +7,13 @@
 
 from django.conf.urls import url
 from jobs import views
+from django.urls import path
 
 urlpatterns = [
     #职位列表
     url(r"^joblist",views.joblist,name='joblist'),
     #职位详情
-    url(r"^job/(?P<job_id>\d+)/$",views.detail,name='detail')
+    url(r"^job/(?P<job_id>\d+)/$",views.detail,name='detail'),
+    #提交简历
+    path('resume/add/',views.ResumeCreateView.as_view(),name='resume-add')
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -6,8 +6,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.http import Http404
-from jobs.models import Job
+from jobs.models import Job,Resume
 from jobs.models import cities,JobTypes
+from django.views.generic.edit import CreateView
+from django.http import HttpResponseRedirect
 
 def joblist(request):
     job_list = Job.objects.order_by('job_type') #django的models的内置方法
@@ -26,3 +28,28 @@ def detail(request,job_id): #jobid 通过变量传进来
         raise Http404("Job does not exist")
 
     return render(request,"job.html",{'job':job})
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class ResumeCreateView(LoginRequiredMixin,CreateView):
+    '''简历职位页面'''
+    template_name = 'resume_form.html'
+    success_url = '/joblist'
+    model = Resume
+    fields = ["username", "city", "phone",
+              "email", "apply_position", "gender",
+              "bachelor_school", "master_school", "major", "degree",
+              "candidate_introduction", "work_experience", "project_experience"]
+    #从url请求参数带入默认值
+    def get_initial(self):
+        initial = {}  #设置了初始值
+        for  x in self.request.GET:
+            initial[x] = self.request.GET[x]
+        return initial
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.applicant = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
